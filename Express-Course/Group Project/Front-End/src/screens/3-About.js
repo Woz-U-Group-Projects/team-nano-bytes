f201ffd592cc1f88d2543eacae3b5031261555ba
GayLyn <glj.dev@gmail.com>
2018-12-04 06:30:16 -0600

    Added some content to the About page

--- Express-Course/Group Project/Front-End/src/screens/3-About.js
+++ Express-Course/Group Project/Front-End/src/screens/3-About.js
@@ -3,7 +3,17 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h2>About</h2>    
+    <h2>About the website</h2>
+
+      <p></p>
+
+    <h2>About the team</h2>
+      <p>"Our World Is So Beautiful" was started by a group of students called Team nano Bytes as a team project. The team consists of four students, Johnny Rodriguez, Meeli Sonn, Ashley Warren, and Amy Jaramillo.</p> 
+
+    <h2>Contact Info</h2> 
+      <p></p>
+
+
   </div>
 );
 
