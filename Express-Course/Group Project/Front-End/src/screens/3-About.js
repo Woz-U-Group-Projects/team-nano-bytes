e47ea585e8714c8202b7a06a817810d691d7d3dc
GayLyn <glj.dev@gmail.com>
2018-12-11 18:26:34 -0600

    Added some style need to make responsive.

--- Express-Course/Group Project/Front-End/src/screens/3-About.js
+++ Express-Course/Group Project/Front-End/src/screens/3-About.js
@@ -1,17 +1,10 @@
 
 import React, { Component } from 'react';
 import '../screens/3-About.css';
-<<<<<<< HEAD
-/*import johnny from './assets/images/johnny.png';
-import ashley from './assets/images/ashley.png';
-import meeli from './assets/images/meeli.png';
-import gaylyn from './assets/images/gaylyn.png'; */
-=======
 import johnny from '../assets/images/johnny.png';
 import ashley from '../assets/images/ashley.png';
 import meeli from '../assets/images/meeli.png';
 import gaylyn from '../assets/images/gaylyn.png'; 
->>>>>>> 587bacddb895657665023631c671f536b9102417
 
 const About = () => (
   <div className="aboutPage">
@@ -20,35 +13,54 @@ const About = () => (
     <div className="website">
       <h2>The Website:</h2>
 
-      <p>Something about the site here.</p>
+      <p className="info">Something about the site here.</p>
     </div>
 
     <div className="team">
       <h2>The Team:</h2>
-      <p>"Our World Is So Beautiful" was started by a group of students called Team Nano Bytes as a team project. The team consists of four students: Johnny Rodriguez, Meeli Sonn, Ashley Warren, and GayLyn Jaramillo.</p> 
+      <p className="info">"Our World Is So Beautiful" was started by a group of students called Team Nano Bytes as a team project. The team consists of four students: Johnny Rodriguez, Meeli Sonn, Ashley Warren, and GayLyn Jaramillo.</p> 
     </div>
 
-    <div className="members">
-    
+    <div className="members">   
       <h2>Team Members:</h2>
-       <img src={johnny} alt="JohnnyRodrguez" width= "200" height= "235" />      
-          <p>Johnny Rodriguez</p>
-          <p>Front-End and Style</p>        
-        
-        <img id="ashley" width= "200" height= "235" alt="AshleyWarren" src={ashley}/>    
-          <p>Ashley Warren</p>
-          <p>Back-End and Database</p>        
-        
-        
 
-        <img id="meeli" width= "200" height= "235" alt="MeeliSonn" src={meeli}/>  
+      <div id="member">
+          <img src={johnny} alt="JohnnyRodrguez"  />
+            <p id="name">Johnny Rodriguez</p>        
+        <div id="info">            
+            <p id="code">Front-End and Style</p> 
+            <p id="about">Some information about Johnny goes here.</p>  
+        </div> 
+      </div>   
+     
 
-          <p>Meeli Sonn</p>
-          <p>Back-End and Database</p>      
+      <div id="member">   
+          <img id="ashley"  alt="AshleyWarren" src={ashley}/>
+            <p id="name">Ashley Warren</p>        
+        <div id="info">                
+            <p id="code">Back-End and Database</p>
+            <p id="about">Some information about Ashley goes here.</p> 
+        </div>       
+      </div>  
         
-        <img id="gaylyn" width= "200" height= "235" alt="GayLynJaramillo" src={gaylyn}/>           
-          <p>GayLyn Jaramillo</p>
-          <p>Front-End and Style <br/> Began working with 3D modeling and photo editing programs as a hobby.<br/> Wanting to further her knowledge, in 2018 she began taking software development course with Woz U. </p>
+      <div id="member">
+          <img id="meeli"  alt="MeeliSonn" src={meeli}/>
+            <p id="name">Meeli Sonn</p>        
+        <div id="info">            
+            <p id="code">Back-End and Database</p> 
+            <p id="about">Some information about Meeli goes here.</p> 
+        </div>
+      </div>    
+        
+      <div id="member">  
+          <img id="gaylyn" alt="GayLynJaramillo" src={gaylyn}/> 
+            <p id="name">GayLyn Jaramillo</p>      
+        <div id="info">            
+            <p id="code">Front-End and Style</p>
+            <p id="about">Began working with 3D modeling and photo editing programs as a hobby.<br/> Wanting to further her knowledge, in 2018 she began taking software development course with Woz U. </p>
+        </div>
+      </div>
+
       </div>
 
     <div className="contact">
