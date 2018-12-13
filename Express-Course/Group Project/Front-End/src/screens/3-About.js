4b2b37363654acad4b38c8073086a8975fd645de
Unknown <craftaddict3205@gmail.com>
2018-12-13 15:21:08 -0600

    Changed code in About page. Still need to make some responsive.

--- Express-Course/Group Project/Front-End/src/screens/3-About.js
+++ Express-Course/Group Project/Front-End/src/screens/3-About.js
@@ -11,8 +11,7 @@ const About = () => (
 
     <div className="website">
       <h2>The Website:</h2>
-
-      <p className="info">Something about the site here.</p>
+      <p className="info">Something about the site here.</p>      
     </div>
 
     <div className="team">
@@ -20,47 +19,45 @@ const About = () => (
       <p className="info">"Our World Is So Beautiful" was started by a group of students called Team Nano Bytes as a team project. The team consists of four students: Johnny Rodriguez, Meeli Sonn, Ashley Warren, and GayLyn Jaramillo.</p> 
     </div>
 
-    <div className="members">   
+       
       <h2>Team Members:</h2>
 
       <div id="member">
-          <img src={johnny} alt="JohnnyRodrguez"  />
+          <img className="photo" src={johnny} alt="JohnnyRodrguez"  />
             <p id="name">Johnny Rodriguez</p>        
-        <div id="info">            
+      </div>   
+      <div id="info">            
             <p id="code">Front-End and Style</p> 
             <p id="about">Some information about Johnny goes here.</p>  
-        </div> 
-      </div>   
-     
+      </div> 
 
       <div id="member">   
-          <img id="ashley"  alt="AshleyWarren" src={ashley}/>
+          <img className="photo" alt="AshleyWarren" src={ashley}/>
             <p id="name">Ashley Warren</p>        
-        <div id="info">                
+      </div>  
+      <div id="info">                
             <p id="code">Back-End and Database</p>
             <p id="about">Some information about Ashley goes here.</p> 
-        </div>       
-      </div>  
-        
+      </div> 
+
       <div id="member">
-          <img id="meeli"  alt="MeeliSonn" src={meeli}/>
+          <img className="photo"  alt="MeeliSonn" src={meeli}/>
             <p id="name">Meeli Sonn</p>        
-        <div id="info">            
+      </div>    
+      <div id="info">            
             <p id="code">Back-End and Database</p> 
             <p id="about">Some information about Meeli goes here.</p> 
-        </div>
-      </div>    
-        
-      <div id="member">  
-          <img id="gaylyn" alt="GayLynJaramillo" src={gaylyn}/> 
-            <p id="name">GayLyn Jaramillo</p>      
-        <div id="info">            
-            <p id="code">Front-End and Style</p>
-            <p id="about">Began working with 3D modeling and photo editing programs as a hobby.<br/> Wanting to further her knowledge, in 2018, she began taking software development course with Woz U. </p>
-        </div>
       </div>
 
+      <div id="member">  
+          <img className="photo" alt="GayLynJaramillo" src={gaylyn}/> 
+            <p id="name">GayLyn Jaramillo</p>        
       </div>
+      <div id="info">            
+            <p id="code">Front-End and Style</p>
+            <p id="about">Began working with 3D modeling and photo editing programs as a hobby.<br/> Wanting to further her knowledge, in 2018, she began taking software development course with Woz U. </p>
+      </div>
+      
 
     <div className="contact">
       <h2>Contact Info:</h2> 
