ce96cd1fb72ddd946ff80ac614cf50de3ec9155d
Unknown <jrod4work@hotmail.com>
2018-12-15 09:39:38 -0700

    Style

--- Express-Course/Group Project/Front-End/src/App.js
+++ Express-Course/Group Project/Front-End/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./screens/1-Home";
 import Portfolio from "./screens/2-Portfolio";
 import About from "./screens/3-About";
-import Topics from './screens/Topics';
 import './App.css';
 import './1-Navbar.css';
 /*import johnny from './assets/images/johnny.png';
@@ -27,9 +26,7 @@ const App = () => (
         <li>
           <Link to="/about">About</Link>
         </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
+       
 
       </ul>
 
@@ -38,7 +35,6 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route path="/portfolio" component={Portfolio} />
       <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
 
     </div>
   </Router>
